8f8d5c4a03f2ba515dfb0ff90649643e16cb030c
Alex Psitos <apsitos08@gmail.com>
2022-03-17 17:35:22 -0400

    Finish practice

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index' #any time we receive an HTTP GET request for the URI '/', we should perform the index action within the welcome controller.
+  get '/tasks', to: 'tasks#index' #how to handle a GET './tasks' request
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
+end
